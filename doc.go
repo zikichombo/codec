0ef255689be52eebc5d312bc2c90bf30f8f7e452
Scott <wsc@iri-labs.com>
2018-08-07 23:24:21 +0200

    add link to discussion for generic codec interface

--- doc.go
+++ doc.go
@@ -2,4 +2,6 @@
 // code is governed by a license that can be found in the License file.
 
 // Package codec supplies pluggable sound codecs.
+//
+// attention: open discussion on design in progress. See https://github.com/zikichombo/codec/issues/3
 package codec /* import "zikichombo.org/codec" */
