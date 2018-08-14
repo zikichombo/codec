ee92437df71c0dd4c535c967d616ab7800de517c
Scott <wsc@iri-labs.com>
2018-08-14 19:16:01 +0200

    discussion followup; untested but builds

--- codec.go
+++ codec.go
@@ -11,7 +11,7 @@ import (
 	"zikichombo.org/sound/sample"
 )
 
-// ErrUnknownCodec is an error representing
+// ErrUnknownCodec is an error representing that a codec is unknown.
 var ErrUnknownCodec = errors.New("unknown codec")
 
 // ErrUnsupportedSampleCodec can be used by codec implementations
@@ -20,8 +20,7 @@ var ErrUnknownCodec = errors.New("unknown codec")
 var ErrUnsupportedSampleCodec = errors.New("unsupported sample codec")
 
 // AnySampleCodec is a sample.Codec which is used as a wildcard sample.Codec
-// in this interface.  Codecs which are not based on PCM data, such as aac/mp3/ogg/vorbis
-//
+// in this interface.
 var AnySampleCodec = sample.Codec(-1)
 
 // Codec represents a way of encoding and decoding sound.
@@ -30,27 +29,33 @@ type Codec struct {
 	// matches an encoding or decoding demand.  Filename extensions can trigger
 	// demands for encoding and decoding.  Magic triggers demands for decoding
 	// All demands may be further qualified by specifying a sample.Codec, with
+	// DefaultSampleCodec being the default.
 	//
 	Priority int
 
 	// Extensions lists the filename extensions which this codec claims to support.
-	// Examples are .wav, .ogg, .caf.
+	// Examples are .wav, .ogg, .caf.  The extension string includes the leading '.'.
 	Extensions []string // Filename extensions
 
-	// Magic represents the first few bytes, such as fLaC.
-	Magic string // Magic represents the first few bytes often
+	// Sniff is a function which when provided with a *bufio.Reader r, may
+	// call r.Peek(), and only r.Peek().  Sniff should return true only if
+	// this codec has a Decoder function for the data based on the data
+	// aquired via r.Peek().
+	Sniff func(*bufio.Reader) bool
 
 	// DefaultSampleCodec gives a default or preferred sample codec for the codec.
 	// Some codecs, such as perception based compressed codecs, don't really have
-	// a defined sample.Codec and should use AnySampleCodec for this field.
-	DefaultSampleCodec sample.Codec // DefaultSampleCodec
+	// a defined sample.Codec and should use AnySampleCodec for this field. Codecs
+	// which have only a decoder should also have AnySampleCodec in this field.
+	DefaultSampleCodec sample.Codec
 
 	// Decoder tries to turn an io.ReadCloser into a sound.Source.  In the event
-	// the decoder does not use a defined sample.Codec, the second return
-	// value should be AnySampleCodec if the error return value is nil.
+	// the decoder does not use a single defined sample.Codec during the entire
+	// decoding process for the resulting sound.Source, then the second return
+	// value should be AnySampleCodec (if the error return value is nil).
 	Decoder func(io.ReadCloser) (sound.Source, sample.Codec, error)
 
-	// Encoder tries to turn a writeCloser into a sound.Sink.
+	// Encoder tries to turn an io.WriteCloser into a sound.Sink.
 	// The sample.Codec argument can specify the desired sample Codec.
 	// For encodings which don't use a defined sample.Codec, the function
 	// should return (nil, ErrUnsupportedSampleCodec) in the event c
@@ -83,34 +88,11 @@ func RegisterCodec(c *Codec) {
 
 	codecs = append(codecs, Codec{Priority: c.Priority,
 		Extensions:         exts,
-		Magic:              c.Magic,
+		Sniff:              c.Sniff,
 		DefaultSampleCodec: c.DefaultSampleCodec,
 		Decoder:            c.Decoder,
 		Encoder:            c.Encoder,
 		RandomAccess:       c.RandomAccess})
-}
-
-type sniffReader interface {
-	io.ReadCloser
-	Peek(int) ([]byte, error)
-}
-
-type sr struct {
-	rc io.ReadCloser
-	*bufio.Reader
-}
-
-func (r *sr) Close() error {
-	return r.Close()
-}
-
-func asSniffReader(r io.ReadCloser) sniffReader {
-	if sr, ok := r.(sniffReader); ok {
-		return sr
-	}
-	return &sr{
-		Reader: bufio.NewReader(r),
-		rc:     r}
 }
 
 // CodecFor tries to find a codec based on a filename extension.
@@ -122,7 +104,7 @@ func asSniffReader(r io.ReadCloser) sniffReader {
 // value.  In case of confict taking into account the priority, the first codec
 // from a call to RegisterCodec is returned.  As this might depend on package
 // initialisation order, it is recommended to Codec implementations intended
-// for library use use even valued priorities so a given application (with a
+// for library use to use even valued priorities so a given application (with a
 // main()) may override package initialistion order by using an appropriate odd Priority
 // value.
 func CodecFor(ext string) (*Codec, error) {
@@ -134,6 +116,7 @@ func CodecFor(ext string) (*Codec, error) {
 			if ext == codExt {
 				if c.Priority < minPriority {
 					bestCodec = c
+					minPriority = c.Priority
 				}
 			}
 		}
@@ -144,13 +127,48 @@ func CodecFor(ext string) (*Codec, error) {
 	return bestCodec, nil
 }
 
-// Decoder
+// Decoder tries to turn an io.ReadCloser into a sound.Source.
+// If it fails, it returns a non-nil error.
+//
+// If it succeeds, it also returns a sample.Codec which may either
+// be:
+//
+// 1. AnySampleCodec, indicating there is no fixed sample codec for
+// the decoder behind sound.Source; or
+// 2. A sample.Codec which defined the data received in sound.Source.
 func Decoder(r io.ReadCloser) (sound.Source, sample.Codec, error) {
-	return nil, sample.SFloat32L, nil
+	br := bufio.NewReader(r)
+	minPriority := int(math.MaxInt32)
+	var theCodec *Codec
+	for i := range codecs {
+		c := &codecs[i]
+		if c.Sniff != nil && c.Sniff(br) && c.Priority < minPriority {
+			minPriority = c.Priority
+			theCodec = c
+		}
+	}
+	if theCodec == nil {
+		return nil, AnySampleCodec, ErrUnknownCodec
+	}
+	return theCodec.Decoder(r)
 }
 
-// Encoder
-func Encoder(dst io.WriteCloser, ext string, c sample.Codec) (sound.Sink, error) {
+// Encoder tries to turn an io.WriteCloser into a sound.Sink
+// given a filename extension.
+func Encoder(dst io.WriteCloser, ext string) (sound.Sink, error) {
+	co, err := CodecFor(ext)
+	if err != nil {
+		return nil, err
+	}
+	return co.Encoder(dst, co.DefaultSampleCodec)
+}
+
+// EncoderWith tries to turn an io.WriteCloser into a sound.Sink
+// given a filename extension and a sample.Codec.
+//
+// The sample codec may be AnySampleCodec, which should be used when
+// the caller is not sure of the desired sample codec c.
+func EncoderWith(dst io.WriteCloser, ext string, c sample.Codec) (sound.Sink, error) {
 	co, err := CodecFor(ext)
 	if err != nil {
 		return nil, err
@@ -158,14 +176,17 @@ func Encoder(dst io.WriteCloser, ext string, c sample.Codec) (sound.Sink, error)
 	return co.Encoder(dst, c)
 }
 
-// Encode
+// Encode encodes a sound.Source to an io.WriteCloser, selecting
+// the codec based on a filename extension ext. It returns any
+// error that may have been encountered in that process.
 func Encode(dst io.WriteCloser, src sound.Source, ext string) error {
 	return EncodeWith(dst, src, ext, AnySampleCodec)
 }
 
-// EncodeWith
+// EncodeWith encodes a sound.Source to an io.WriteCloser, selecting
+// the codec based on a filename extension ext and desired sample codec co.
 func EncodeWith(dst io.WriteCloser, src sound.Source, ext string, co sample.Codec) error {
-	snk, err := Encoder(dst, ext, co)
+	snk, err := EncoderWith(dst, ext, co)
 	if err != nil {
 		return err
 	}
