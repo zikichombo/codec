8713f57d992e2098f649c1c34b9a5c37ad2842c6
Scott <wsc@iri-labs.com>
2018-09-10 16:30:07 +0200

    empty tests

---
+++ codec_test.go
@@ -0,0 +1,19 @@
+package codec
+
+import "testing"
+
+// TBD
+func TestRegister(t *testing.T) {
+}
+
+func TestDecoder(t *testing.T) {
+}
+
+func TestSeekingDecoder(t *testing.T) {
+}
+
+func TestEncoder(t *testing.T) {
+}
+
+func TestRandomAccess(t *testing.T) {
+}
